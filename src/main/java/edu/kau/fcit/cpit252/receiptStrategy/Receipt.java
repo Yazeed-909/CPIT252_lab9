16b4d1a061a4595b1892c697e38dd3263b818ab3
yzeed909 <63503810+yzeed909@users.noreply.github.com>
2022-04-19 21:11:48 +0300

    Add files via upload

---
+++ src/main/java/edu/kau/fcit/cpit252/receiptStrategy/Receipt.java
@@ -0,0 +1,43 @@
+package edu.kau.fcit.cpit252.receiptStrategy;
+
+import edu.kau.fcit.cpit252.paymentsStrategy.Payment;
+import edu.kau.fcit.cpit252.shopping.Product;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public abstract class Receipt {
+    private String id;
+    private List<Product> products;
+    private String issueDate;
+    public  String subject;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public String getIssueDate() {
+        return issueDate;
+    }
+
+    public Receipt(String subject ,List<Product> products){
+        this.subject = subject;
+        this.id = UUID.randomUUID().toString();
+        this.products = products;
+        String datePattern = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(datePattern);
+        this.issueDate = simpleDateFormat.format(new Date());
+    }
+    public abstract void generate();
+
+}
